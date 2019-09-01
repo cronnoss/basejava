bc4054c18c998f687ff56cf2f8f84ced5fe1c42f
Evgeniy Karasev <karasevevg@gmail.com>
2019-09-01 11:17:13 +1000

    introduce pakage

---
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -0,0 +1,59 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void update(Resume r) {
+        // TODO check if resume present
+    }
+
+    public void save(Resume r) {
+        // TODO check if resume not present
+        storage[size] = r;
+        size++;
+    }
+
+    public Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return storage[i];
+            }
+        }
+        return null;
+    }
+
+    public void delete(String uuid) {
+        // TODO check if resume present
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
+            }
+        }
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    public int size() {
+        return size;
+    }
+}
