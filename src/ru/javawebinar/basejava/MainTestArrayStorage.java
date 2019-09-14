4f51952ee7600dc7bf0d858ea778a98e29c0b3c8
Evgeniy Karasev <karasevevg@gmail.com>
2019-09-14 14:40:54 +1000

    update Lesson03 HW. Sorted uuid with index no more then 10

--- src/ru/javawebinar/basejava/MainTestArrayStorage.java
+++ src/ru/javawebinar/basejava/MainTestArrayStorage.java
@@ -10,22 +10,16 @@ public class MainTestArrayStorage {
     private static final SortedArrayStorage ARRAY_STORAGE = new SortedArrayStorage();
 
     public static void main(String[] args) {
-        final Resume r4 = new Resume();
-        r4.setUuid("uuid4");
+        final Resume r1 = new Resume();
+        r1.setUuid("uuid1");
         final Resume r2 = new Resume();
         r2.setUuid("uuid2");
         final Resume r3 = new Resume();
         r3.setUuid("uuid3");
-        final Resume r1 = new Resume();
-        r1.setUuid("uuid1");
-        final Resume r22 = new Resume();
-        r22.setUuid("uuid22");
 
-        ARRAY_STORAGE.save(r4);
+        ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
-        ARRAY_STORAGE.save(r1);
-        ARRAY_STORAGE.save(r22);
 
         System.out.println("Get r2: " + ARRAY_STORAGE.get(r2.getUuid()));
         System.out.println("Size: " + ARRAY_STORAGE.size());
@@ -35,7 +29,7 @@ public class MainTestArrayStorage {
         ARRAY_STORAGE.update(r2);
         printAll();
 
-        ARRAY_STORAGE.delete(r2.getUuid());
+        ARRAY_STORAGE.delete(r1.getUuid());
         printAll();
         ARRAY_STORAGE.clear();
         printAll();
