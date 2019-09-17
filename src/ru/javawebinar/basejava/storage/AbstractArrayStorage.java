7939008e98a8af4aa60050fce480777fe5ba970a
Evgeniy Karasev <karasevevg@gmail.com>
2019-09-17 23:18:17 +1000

    The implementation of the template method. Second attempt.

--- src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractArrayStorage.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public abstract class AbstractArrayStorage implements Storage {
+
     protected static final int STORAGE_LIMIT = 10000;
 
     protected Resume[] storage = new Resume[STORAGE_LIMIT];
@@ -24,7 +25,7 @@ public abstract class AbstractArrayStorage implements Storage {
 
     public void update(Resume r) {
         int index = getIndex(r.getUuid());
-        if (!uuidExist(r)) {
+        if (index < 0) {
             System.out.println("Resume " + r.getUuid() + " not exist");
         } else {
             storage[index] = r;
@@ -32,31 +33,18 @@ public abstract class AbstractArrayStorage implements Storage {
         }
     }
 
-    public void save(Resume r) {
-        if (uuidExist(r)) {
-            System.out.println("Resume " + r.getUuid() + " already exist");
-        } else if (size >= STORAGE_LIMIT) {
-            System.out.println("Storage overflow");
-        } else {
-            saveResume(r);
-        }
-    }
+    public abstract void save(Resume r);
 
     public Resume get(String uuid) {
-        if (getIndex(uuid) < 0) {
+        int index = getIndex(uuid);
+        if (index < 0) {
             System.out.println("Resume " + uuid + " not exist");
             return null;
         }
-        return storage[getIndex(uuid)];
+        return storage[index];
     }
 
-    public void delete(String uuid) {
-        if (getIndex(uuid) < 0) {
-            System.out.println("Resume " + uuid + " not exist");
-        } else {
-            deleteResume(uuid);
-        }
-    }
+    public abstract void delete(String uuid);
 
     public Resume[] getAll() {
         return Arrays.copyOfRange(storage, 0, size);
@@ -64,9 +52,4 @@ public abstract class AbstractArrayStorage implements Storage {
 
     protected abstract int getIndex(String uuid);
 
-    protected abstract boolean uuidExist(Resume r);
-
-    public abstract void saveResume(Resume r);
-
-    public abstract void deleteResume(String uuid);
 }
--- src/ru/javawebinar/basejava/storage/ArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/ArrayStorage.java
@@ -7,7 +7,28 @@ import ru.javawebinar.basejava.model.Resume;
  */
 public class ArrayStorage extends AbstractArrayStorage {
 
-    @Override
+    public void save(Resume r) {
+        if (getIndex(r.getUuid()) >= 0) {
+            System.out.println("Resume " + r.getUuid() + " already exist");
+        } else if (size >= STORAGE_LIMIT) {
+            System.out.println("Storage overflow");
+        } else {
+            storage[size] = r;
+            size++;
+        }
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index < 0) {
+            System.out.println("Resume " + uuid + " not exist");
+        } else {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        }
+    }
+
     protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (uuid.equals(storage[i].getUuid())) {
@@ -15,28 +36,5 @@ public class ArrayStorage extends AbstractArrayStorage {
             }
         }
         return -1;
-    }
-
-    @Override
-    public boolean uuidExist(Resume r) {
-        int index = getIndex(r.getUuid());
-        if (index == -1) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public void saveResume(Resume r) {
-        storage[size] = r;
-        size++;
-    }
-
-    @Override
-    public void deleteResume(String uuid) {
-        int index = getIndex(uuid);
-        storage[index] = storage[size - 1];
-        storage[size - 1] = null;
-        size--;
     }
 }
