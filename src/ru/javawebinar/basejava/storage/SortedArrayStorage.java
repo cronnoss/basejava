a569cc00b136dbab3651b513cc56fd03cd09afea
Evgeniy Karasev <karasevevg@gmail.com>
2019-09-20 08:50:35 +1000

    deleted condition if (size - 1 - index >= 0)

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -15,9 +15,7 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected void deleteResume(int index) {
-        if (size - 1 - index >= 0) {
-            System.arraycopy(storage, index + 1, storage, index, size - 1 - index);
-        }
+        System.arraycopy(storage, index + 1, storage, index, size - 1 - index);
     }
 
     @Override
