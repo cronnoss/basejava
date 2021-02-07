eb3534a0dd1e5e2706a4fe5f089298d86efd40b6
Evgeniy Karasev <karasevevg@gmail.com>
2021-02-07 22:51:54 +0300

    Lesson10 xml

--- src/ru/javawebinar/basejava/model/OrganizationSection.java
+++ src/ru/javawebinar/basejava/model/OrganizationSection.java
@@ -1,13 +1,19 @@
 package ru.javawebinar.basejava.model;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import java.util.List;
 import java.util.Objects;
 
+@XmlAccessorType(XmlAccessType.FIELD)
 public class OrganizationSection extends Section {
 
     private static final long serialVersionUID = 1L;
 
-    private final List<Organization> organizations;
+    private List<Organization> organizations;
+
+    public OrganizationSection() {
+    }
 
     public OrganizationSection(List<Organization> organizations) {
         Objects.requireNonNull(organizations, "organizations must not be null");
