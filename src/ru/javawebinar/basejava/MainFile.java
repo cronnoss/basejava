0498d7bc1a823550698ce7f1118edc081443a5af
Evgeniy Karasev <karasevevg@gmail.com>
2021-02-07 20:19:41 +0300

    Lesson10 HW09

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -26,20 +26,20 @@ public class MainFile {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        printDirectoryDeeply(dir);
+        printDirectoryDeeply(dir, "");
     }
 
     // TODO: make pretty output
-    public static void printDirectoryDeeply(File dir) {
+    public static void printDirectoryDeeply(File dir, String offset) {
         File[] files = dir.listFiles();
 
         if (files != null) {
             for (File file : files) {
                 if (file.isFile()) {
-                    System.out.println("File: " + file.getName());
+                    System.out.println(offset +  "File: " + file.getName());
                 } else if (file.isDirectory()) {
                     System.out.println("Directory: " + file.getName());
-                    printDirectoryDeeply(file);
+                    printDirectoryDeeply(file, offset + "  ");
                 }
             }
         }
