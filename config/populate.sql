f5903d6d34e056866b0fa41475d846c2a0f1eec5
Evgeniy Karasev <karasevevg@gmail.com>
2021-02-14 22:59:37 +0300

    Lesson14 HW13

---
+++ config/populate.sql
@@ -0,0 +1,4 @@
+INSERT INTO resume (uuid, full_name)
+VALUES ('71baa6ee-482a-4ab1-99b2-569adb4c124f', 'Name1'),
+       ('73ddf3b7-ed47-4b12-ae9b-6c30dfcbd91b', 'Name2'),
+       ('5393337b-bf46-417f-b544-1dbde680e70b', 'Name3');
