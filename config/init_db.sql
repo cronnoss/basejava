9445baee753c7fff274af167163355bfb42a44df
Evgeniy Karasev <karasevevg@gmail.com>
2021-02-14 11:00:12 +0300

    Lesson13 Init DB

---
+++ config/init_db.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS resume
+(
+    uuid      CHAR(36) PRIMARY KEY NOT NULL,
+    full_name TEXT                 NOT NULL
+);
+ALTER TABLE resume
+    OWNER TO postgres;
+
+CREATE TABLE IF NOT EXISTS contact
+(
+    id          SERIAL,
+    resume_uuid CHAR(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
+    type        TEXT     NOT NULL,
+    value       TEXT     NOT NULL
+);
+
+CREATE UNIQUE INDEX contact_uuid_type_index
+    ON contact (resume_uuid, type);
+
+ALTER TABLE contact
+    OWNER TO postgres;
