80b20d149a6169a14cf26e55a6cfd62ad32c692b
Evgeniy Karasev <karasevevg@gmail.com>
2021-02-14 17:50:30 +0300

    Lesson13 SqlStorage

--- test/ru/javawebinar/basejava/storage/AbstractStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AbstractStorageTest.java
@@ -39,7 +39,7 @@ public abstract class AbstractStorageTest {
         R3 = new Resume(UUID_3, "Name3");
         R4 = new Resume(UUID_4, "Name4");
 
-        R1.addContact(ContactType.MAIL, "mail1@ya.ru");
+        /*R1.addContact(ContactType.MAIL, "mail1@ya.ru");
         R1.addContact(ContactType.PHONE, "11111");
         R1.addSection(SectionType.OBJECTIVE, new TextSection("Objective1"));
         R1.addSection(SectionType.PERSONAL, new TextSection("Personal data"));
@@ -86,7 +86,7 @@ public abstract class AbstractStorageTest {
                                         LocalDate.of(2015, 1, 1),
                                         NOW,
                                         "position1",
-                                        "content1")))));
+                                        "content1")))));*/
     }
 
     protected AbstractStorageTest(Storage storage) {
